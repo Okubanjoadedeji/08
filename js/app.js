4b03083b2c3ca4543b909fed3993740f1231ff86
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-18 18:35:15 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+OR ||   => ATLEAST ONE OPERAND IS TRUE => TRUE
+console.log(true || true); //true
+console.log(true || false); //true
+console.log(false || false); //false
+console.log(false || true); //true
+
+NOT ! => OPPOSITE OF ORIGINAL OPERAND TRUE => FALSE, FALSE => TRUE
+console.log(!true); //false
+console.log(!false); //ture
